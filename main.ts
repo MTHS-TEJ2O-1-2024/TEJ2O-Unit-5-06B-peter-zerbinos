34c166aa8691813e367c49d6500148205cbb2a22
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-16 11:07:53 -0400

    distance code added

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+*
+* Created by: Mr. Coxall  
+* Created on: Oct 2024 
+* This program finds the distance using a sonar 
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup 
+basic.showIcon(IconNames.Happy)
+
+// find distance from sonar 
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2, 
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy) 
+})
